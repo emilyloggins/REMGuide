21710fe6dd7044313baec8e508f6643bc55135ab
Emily Loggins <em.loggins7@gmail.com>
2019-09-10 10:39:53 -0500

    checkbox model and partial view

---
+++ REMGuide/Models/ViewModels/CreateEntryViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace REMGuide.Models.ViewModels
+{
+    public class CreateEntryViewModel
+    {
+        public List<SelectListItem> Cohorts { get; set; }
+        public Student Student { get; set; }
+    }
+}
